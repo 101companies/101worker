c23736fa066bb37b69cad750373da4722d9327f9
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2012-04-22 16:47:07 +0200

    category tree module added

--- configs/wikibot.config.php
+++ configs/wikibot.config.php
@@ -1,6 +1,6 @@
 <?php
 $wikibotSetting['user']['TestBot']['Testwiki'] = 'TestBot';
-$wikibotSetting['pass']['TestBot']['Testwiki'] = 'password';
+$wikibotSetting['pass']['TestBot']['Testwiki'] = '';
 $wikibotSetting['apiurl']['*']['101companies'] = 'http://sl-mac.uni-koblenz.de/~wiki101/wiki/api.php';
 $wikibotSetting['queryurl']['*']['101companies'] = 'http://sl-mac.uni-koblenz.de/~wiki101/wiki/query.php';
 $wikibotSetting['indexurl']['*']['101companies'] = 'http://sl-mac.uni-koblenz.de/~wiki101/wiki/index.php';
