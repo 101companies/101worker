524540ff3a72aa7f4d4becb96254f4a7f4e56a94
megaplanet <megaplanet.github@megaplanet.org>
2012-04-24 03:01:34 +0200

    Added support for sparql endpoint

--- modules/101WikiJsonTo101Rdf/Json101AsRDF101.php
+++ modules/101WikiJsonTo101Rdf/Json101AsRDF101.php
@@ -17,16 +17,25 @@ $graphasrdf->addERGraph(
     $graph,RDF_DATA_101_PREFIX_URL,RDF_SCHEMA_101_PREFIX_URL) ;
 
 // Save the triples in different file formats
-if (DEBUG) echo "<h2>Saving the triples in files</h2>" ;
+if (DEBUG) echo "======= Saving the triples in files =========\n" ;
 $tripleset = $graphasrdf->getTripleSet() ;
 $formats='HTML,GraphML,Graphviz,Turtle,RDFXML,RDFJSON,NTriples' ;
 $tripleset->saveFiles($formats,RDF_WIKI_101_DATA_GENERATED_CORE_FILENAME) ;
 
 
 
-if (DEBUG) echo "<h2>Saving the triples in the RDF store</h2>" ;
+if (DEBUG) echo "======= Saving the triples in the RDF store ========\n" ;
 // Save the triples in the RDF101 store
 $store101 = get101Store() ;
 //$store101->reset() ;
 $graphasrdf->save($store101,RDF_WIKI_101_NAMED_GRAPH) ;
+
+
+if (DEBUG) echo "====== Deploying the end point code to ".RDF_STORE_END_POINT_CODE." =========\n" ;
+$endPointCode =
+  '<?php
+   require_once("../101worker/configs/RDF101.config.php") ;
+   get101Store()->startSparqlEndpoint() ;
+  ' ;   
+file_put_contents(RDF_STORE_END_POINT_CODE,$endPointCode) ;
 
