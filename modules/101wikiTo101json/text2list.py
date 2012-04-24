b735c4e6504bf8251cefe217d81b2d7265eeed76
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2012-04-24 16:46:16 +0200

    New format for links

--- modules/101wikiTo101json/text2list.py
+++ modules/101wikiTo101json/text2list.py
@@ -2,20 +2,24 @@ import sys
 import re
 import json
 
-def text2List(text):
+def text2Lists(text):
 	textstart = 0
+	texts = []
+	links = []
 	for textm in re.finditer("\[\[([^\[\|]+)(\|([^\[]+))?\]\]",text):
 		linkm = re.match("(([^\:]+):)?(.+)", textm.group(1))
-		yield ('text', text[textstart:textm.start()])
-		yield ('link', linkm.group(2), linkm.group(3), textm.group(3))
+		texts.append(text[textstart:textm.start()])
+		links.append((linkm.group(2), linkm.group(3), textm.group(3)))
 		textstart = textm.end()
+	return dict(texts=texts, links=links)
 
 def listify(json, textsections):
 	for t in json:
 		for e in json[t]:
 			for attr in json[t][e]:
 				if attr in textsections:
-					json[t][e][attr] = list(text2List(json[t][e][attr]))				
+					result = text2Lists(json[t][e][attr])
+					json[t][e][attr] = result			
 
 if len(sys.argv) <= 2:
 	print "Need input path of basic json and output for linkified json"
