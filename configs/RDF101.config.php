550650bcd695724e67474943ba588c15465e19e0
megaplanet <megaplanet.github@megaplanet.org>
2012-04-25 19:28:25 +0200

    added variables in Makefile.vars. Change the name of the wikijon -> wiki rdf module. Adapted condig*.php

--- configs/RDF101.config.php
+++ configs/RDF101.config.php
@@ -4,9 +4,6 @@ require_once ABSPATH_MEGALIB.'configs/RDF.config.php' ;
 
 define('RDF_101_STORE_NAME','s101') ;
 
-define('WIKI_101_SCHEMA_URL',ABSPATH_WORKER_CONFIG_DIR.'Wiki101Schema.ers') ;
-define('WIKI_101_ENTITY_JSON_MAPPING_URL',ABSPATH_WORKER_CONFIG_DIR.'Wiki101EntityKindToWiki101JsonTopLevelTag.json') ;
-
 define('DOMAIN_DATA_101','data'.DOMAIN_PROJECT_101) ;
 define('RDF_DATA_101_URL','http://'.DOMAIN_DATA_101.'/data/') ;
 
@@ -19,7 +16,6 @@ define('RDF_SCHEMA_101_PREFIX_URL',RDF_DATA_101_URL.'/schema#') ;
 
 define('RDF_WIKI_101_NAMED_GRAPH',RDF_DATA_101_URL.'dumps/Wiki101Full.rdf') ;
 define('RDF_CODE_101_NAMED_GRAPH',RDF_DATA_101_URL.'dumps/Repo101Full.rdf') ;
-define('RDF_WIKI_101_DATA_GENERATED_CORE_FILENAME',ABSPATH_DATA_GENERATED.'Wiki101Full') ;
 
 define ('RDF_STORE_END_POINT_CODE',ABSPATH_DATA_GENERATED.'sparql.php') ;
  
--- configs/main.config.php
+++ configs/main.config.php
@@ -4,5 +4,4 @@ require_once 'main.config.local.php' ;
 
 define('DOMAIN_PROJECT_101','101companies.org') ;
 define('URL_PROJECT_101_URL','http://'.DOMAIN_PROJECT_101) ;
-define('URL_WIKI_101_JSON_URL',ABSPATH_WORKER_WEB_DIR.'dumps/Wiki101Full.json') ;
 define('ABSPATH_DATA_GENERATED',ABSPATH_WORKER_WEB_DIR.'dumps/') ;
