def4471a956278de91007d9d274fe1c6288c6f8b
megaplanet <megaplanet.github@megaplanet.org>
2012-05-10 23:12:57 +0200

    added checks in pullLibraries module

--- modules/pullLibraries/pullgithub.sh
+++ modules/pullLibraries/pullgithub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USAGE="$0 <githubrepository> <targetdirectory>"
+USAGE="$0 <githubrepository> <absoluteTargetDirectory>"
 GITHUB_REPOSITORY=${1?$USAGE}
 TARGET_DIRECTORY=${2?$USAGE}
 CURRENT_DIRECTORY=`pwd`
@@ -12,19 +12,15 @@ then
   echo "    Directory ${TARGET_DIRECTORY?}/.git exist. "
 else
   echo "    Directory ${TARGET_DIRECTORY?}/.git does not exist."
-  BASEDIR=`dirname ${TARGET_DIRECTORY}` 
-  echo "    Cloning ${GITHUB_REPOSITORY?} into ${BASEDIR?}"
-  cd ${BASEDIR?} 
-  git clone https://github.com/${GITHUB_REPOSITORY?}
+  echo "    Cloning ${GITHUB_REPOSITORY?}"
+  git clone https://github.com/${GITHUB_REPOSITORY?} ${TARGET_DIRECTORY?}
   echo "    clone done"
 fi
 
 #----- pull the repository
 # get back to the directory where the script was before. 
 # this is necessary as the target directory might be relative. 
-cd ${CURRENT_DIRECTORY?}    #
 cd ${TARGET_DIRECTORY?}
 echo "    pulling ${GITHUB_REPOSITORY?} into ${TARGET_DIRECTORY?}"
 git pull 
-cd ${CURRENT_DIRECTORY?}
 echo "    done"
