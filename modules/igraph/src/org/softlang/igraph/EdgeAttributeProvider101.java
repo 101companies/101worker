0a7d044b44814ee5bdb5c1db0d41572319291da9
Andrei Varanovich <dotnetby@gmail.com>
2012-05-12 18:36:46 +0200

    new style

--- modules/igraph/src/org/softlang/igraph/EdgeAttributeProvider101.java
+++ modules/igraph/src/org/softlang/igraph/EdgeAttributeProvider101.java
@@ -3,12 +3,14 @@ package org.softlang.igraph;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jgraph.graph.Edge;
 import org.jgrapht.ext.ComponentAttributeProvider;
 
-public class EdgeAttributeProvider101 implements ComponentAttributeProvider<Object> {
+public class EdgeAttributeProvider101<DefaultEdge> implements ComponentAttributeProvider<DefaultEdge> {
 
 	@Override
-	public Map<String, String> getComponentAttributes(Object arg0) {
+	public Map<String, String> getComponentAttributes(DefaultEdge e) {
+		
 		Map<String,String> attr = new HashMap<String, String>();
 		attr.put("color", "blue");
 		return attr;
