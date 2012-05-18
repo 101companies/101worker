2087c7e28a6d21ab7a89eeeecc2736db28d6d68b
megaplanet <megaplanet.github@megaplanet.org>
2012-05-18 21:22:18 +0200

    corrected bug

--- modules/gatherSourceDirectoryMatchingRules/generateSourceDirectoryMatchingRules.php
+++ modules/gatherSourceDirectoryMatchingRules/generateSourceDirectoryMatchingRules.php
@@ -5,16 +5,16 @@ $sourceRuleFile=$argv[$n++] ;
 $targetRuleFile=$argv[$n++] ;
 
 require_once '../../configs/main.config.local.php' ;
-echo "Using megalib from ".ABSPATH_MEGALIB ;
+echo "Using megalib from ".ABSPATH_MEGALIB."\n" ;
 require_once ABSPATH_MEGALIB.'CSV.php' ;
 require_once ABSPATH_MEGALIB.'Files.php' ;
 
 
 $csv = new CSVFile() ;
 if (!$csv->load($sourceRuleFile)) {
-  die("File $sourceRuleFile not found") ;
+  die("File $sourceRuleFile not found\n") ;
 }
 if(!saveFile($targetRuleFile,$csv->getJSON())) {
-  die('Cannot save file '.$targetRuleFile) ;
+  die('Cannot save file '.$targetRuleFile."\n") ;
 }
 
