484537b4751daee33305e1a4db697efa8dfd8a19
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2012-06-01 00:43:51 +0200

    Fix

--- modules/sourceDirectoryFragmentationAggregation/aggregate.py
+++ modules/sourceDirectoryFragmentationAggregation/aggregate.py
@@ -11,8 +11,7 @@ for contribdir in filter(lambda x: os.path.exists(os.path.join(rootdir, x, "inde
 	for root, subFolders, files in os.walk(os.path.join(rootdir, contribdir)):
 		for file in filter(lambda x: x.endswith(".fragments.json"), files):
 			fullpath = os.path.join(root,file)
-			print fullpath
-			print open(fullpath).read()
 			fragments[os.sep.join(fullpath.split(os.sep)[5:])] = json.loads(open(fullpath).read())
 	print len(fragments), "fragments found."
+	os.remove(os.path.join(rootdir,contribdir, "index.fragments.json")
 	open(os.path.join(rootdir,contribdir, "index.fragments.json"), "r+").write(json.dumps(fragments))
