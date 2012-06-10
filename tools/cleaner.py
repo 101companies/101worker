f155a2622e4904dbb0ddb4de04707573ec5e0669
Andrei Varanovich <dotnetby@gmail.com>
2012-06-10 19:02:37 +0200

    remove pid as a part of cleaning

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -1,9 +1,12 @@
 import sys
 import commands
+import os
 
 if (len(sys.argv) == 2):
    module = sys.argv[1]
    commands.getstatusoutput('cd '+module+'; make clean')
+   if os.path.isfile(module+'/pid'): 
+   	os.remove(module+'/pid')
    sys.exit(0)
 else:
    sys.exit(-1)
