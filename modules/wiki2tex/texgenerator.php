6fdab1a0507811c919800b10b34c8436bfbe3ab3
Andrei Varanovich <dotnetby@gmail.com>
2012-06-12 10:15:59 +0200

    minor fix

--- modules/wiki2tex/texgenerator.php
+++ modules/wiki2tex/texgenerator.php
@@ -95,12 +95,12 @@ $args = CommandLine::parseArgs($_SERVER['argv']);
 
 if($args['mode'] == 'singlePage'){
   $title = $args['title'];
-  if (startsWith("Language:",$title)) {
+  if (startsWith("101implementation:",$title)) {
     echo $title;
-    $page = new LanguagePage($title);
+    $page = new ImplementationPage($title);
     #var_dump($page);
-    echo $page->dumpToTex();
-    #var_dump(formatter::toTex($page->illustration));
+    #echo $page->dumpToTex();
+    var_dump(formatter::toTex($page->illustration));
   }
 
 
