77a5a379047bab4c199d1b656489c0bc955e8f02
Andrei Varanovich <dotnetby@gmail.com>
2012-06-17 19:49:18 +0200

    minor fix

--- modules/wikitools/utils.py
+++ modules/wikitools/utils.py
@@ -21,7 +21,7 @@ def toTex(txt):
   txt = txt.replace("<references>", "")
   txt = txt.replace("<references/>", "")
   txt = txt.replace('^','\^')
-  #txt = txt.replace('\"','\\"')
+  txt = txt.replace('#','\#')
 
   return handleUmlauts(txt) 
 
