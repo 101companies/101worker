375f0043bf28a2900c7024c565ce193f6e67b038
Andrei Varanovich <dotnetby@gmail.com>
2012-06-27 17:06:00 +0200

    minor fix

--- tools/runner.py
+++ tools/runner.py
@@ -19,21 +19,21 @@ def write2moduleLog(msg, module):
 if (len(sys.argv) == 2):
    module = sys.argv[1]
    write2log('\nPerforming module %s.' % module)
-   write2log('Started at % s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+   write2log('\nStarted at % s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
    p = subprocess.Popen('cd '+module+'; make', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-   write2log('Module PID: %s' % p.pid)
+   write2log('\nModule PID: %s' % p.pid)
 
    #creating a PID file is the module subdir indicating the the module is running.
    pidFileName = module + "/pid"; 
 
    #if file exists -- the process is already running, report to the log and skip it
    if os.path.isfile(os.getcwd() + '/' + pidFileName):
-      write2log('Module %s is already running; processId: %s' %(module,str(p.pid)))
+      write2log('\nModule %s is already running; processId: %s' %(module,str(p.pid)))
       sys.exit(0)
 
    pid_file = open(os.getcwd() + '/' + pidFileName, "w")
-   print "Creating PID file: %s" % str(os.getcwd() + '/' + pidFileName)
+   print "\nCreating PID file: %s" % str(os.getcwd() + '/' + pidFileName)
    pid_file.write(str(p.pid))
    pid_file.close()
 
@@ -41,11 +41,11 @@ if (len(sys.argv) == 2):
       write2moduleLog(line, module)
    retval = p.wait()
 
-   write2log('Finished at % s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+   write2log('\nFinished at % s' % strftime("%Y-%m-%d %H:%M:%S", gmtime()))
    #write2log("Retval %s" % retval)
 
    #remove PID file when the process finished
-   write2log("Removing PID file: %s" % str(os.getcwd() + '/' + pidFileName))
+   write2log("\nRemoving PID file: %s" % str(os.getcwd() + '/' + pidFileName))
    os.remove(pidFileName)
 
    if (retval == 0):
