51e3f294fc92842801495592a68313c5e0aef714
Ralf Laemmel <rlaemmel@gmail.com>
2012-07-02 20:20:19 +0200

    Tweaking

--- modules/index101meta/program.py
+++ modules/index101meta/program.py
@@ -17,7 +17,7 @@ def addMetrics(lvalue, rvalue):
 def initializeKey(d,key):
     if not key in d:
         d[key] = dict()
-        d[key]["files"] = set()
+        d[key]["files"] = dict()
         d[key]["metrics"] = const101.noMetrics()
 
 def fun(dirname, dirs, files):
@@ -26,6 +26,9 @@ def fun(dirname, dirs, files):
     technologies = dict()
     indexFile = open(os.path.join(const101.tRoot, dirname, "index.json"), 'w')
 
+    #
+    # Aggregation of file summaries
+    #
     for basename in files:
         summaryFile = open(os.path.join(const101.tRoot, dirname, basename + ".summary.json"), 'r')
         summary = json.load(summaryFile)
@@ -35,7 +38,7 @@ def fun(dirname, dirs, files):
         values = tools101.valuesByKey(summary, "language")
         for key in values:
             initializeKey(languages,key)
-            languages[key]["files"].add(basename)
+            languages[key]["files"][basename] = []
             addMetrics(languages[key],summary)
 
         # Deal with technologies for file
@@ -45,9 +48,12 @@ def fun(dirname, dirs, files):
         values += tools101.valuesByKey(summary, "dependsOn")
         for key in values:
             initializeKey(technologies,key)
-            technologies[key]["files"].add(basename)
+            technologies[key]["files"][basename] = []
             addMetrics(technologies[key],summary)
 
+    #
+    # Aggregation of subdirectory indexes
+    #
     for subdirname in dirs:
         subdirFile = open(os.path.join(const101.tRoot, dirname, subdirname, "index.json"), 'r')
         index = json.load(subdirFile)
@@ -57,20 +63,17 @@ def fun(dirname, dirs, files):
         for key in index["languages"]:
             initializeKey(languages,key)
             for filename in index["languages"][key]["files"]:
-                languages[key]["files"].add(os.path.join(subdirname, filename))                
+                languages[key]["files"][os.path.join(subdirname, filename)] = []              
             addMetrics(languages[key],index["languages"][key])
 
         # Deal with technologies for directory
         for key in index["technologies"]:
             initializeKey(technologies,key)
             for filename in index["technologies"][key]["files"]:
-                technologies[key]["files"].add(os.path.join(subdirname, filename))                
+                technologies[key]["files"][os.path.join(subdirname, filename)] = []       
             addMetrics(technologies[key],index["technologies"][key])
 
-    for key in languages:
-        languages[key]["files"] = list(languages[key]["files"])
-    for key in technologies:
-        technologies[key]["files"] = list(technologies[key]["files"])
+    # Dumping the index
     index = dict()
     index["dirs"] = dirs
     index["files"] = files
