ae16e4ae1e52dbc858ffef4ab7449c829e91dadc
Andrei Varanovich <dotnetby@gmail.com>
2012-07-25 16:02:45 +0200

    Minor fix

--- tools/runner.py
+++ tools/runner.py
@@ -90,7 +90,7 @@ def main(config, is_verbose):
       write2log(msg)
       #remove PID file when the process finished
       #write2log("\nRemoving PID file: %s" % str(os.getcwd() + '/' + pidFileName))
-      os.remove(pidFileName)
+      os.remove(os.getcwd() + '/' + pidFileName)
    
    sys.exit(0)
 
