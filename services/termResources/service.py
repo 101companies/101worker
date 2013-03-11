c1e96d52296e85cf633ac4b0f2c091da5c57e2a5
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 09:47:02 +0100

    New term resouces module

---
+++ services/termResources/service.py
@@ -0,0 +1,21 @@
+def helloName(environ, start_response, params):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/json')]
+    start_response(status, response_headers)
+    open("resources.json", "read").read()
+    return {}
+
+
+def hello(environ, start_response, params):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/json')]
+    start_response(status, response_headers)
+
+    return {}
+
+
+def routes():
+    return [
+        ('/hello/(?P<name>[^/]+)', helloName),
+        ('/hello', hello)
+    ]
