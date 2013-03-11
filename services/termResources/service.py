2d7f1ddc75b5253d792dc9501bc5d8feb89e9d52
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 10:13:58 +0100

    New term resouces module

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -1,25 +1,28 @@
 import json
 
+def serverTermInResource(environ, start_response, params):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/json')]
+    start_response(status, response_headers)
+    return json.dumps({})
+
+
+def withTerm(environ, start_response, params):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/json')]
+    start_response(status, response_headers)
+    return json.dumps({})
+
 def serveResourceNames(environ, start_response, params):
     status = '200 OK'
     response_headers = [('Content-Type', 'text/json')]
     start_response(status, response_headers)
     resourceNames = json.load(open('./resources.json'))['resources']
-
     return json.dumps({'availableResouces' : resourceNames})
-
-
-def hello(environ, start_response, params):
-    status = '200 OK'
-    response_headers = [('Content-Type', 'text/json')]
-    start_response(status, response_headers)
-
-    return json.dumps({})
-
 
 def routes():
     return [
-        ('/termResources/(?P<term>[^/]+/(?P<resource>[^/]+)', withTermInResource),
+        ('/termResources/(?P<term>[^/]+/(?P<resource>[^/]+)', serveTermInResource),
         ('/termResources/(?P<term>[^/]+)', serveTerm),
         ('/termResources', serveResourceNames)
     ]
