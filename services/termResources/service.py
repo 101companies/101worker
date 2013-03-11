57b4b86006bc498558c1d68e9ff147a839884d40
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 10:04:39 +0100

    New term resouces module

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -4,9 +4,9 @@ def helloName(environ, start_response, params):
     status = '200 OK'
     response_headers = [('Content-Type', 'text/json')]
     start_response(status, response_headers)
+    resourceNames = json.load(open('/resources.json'))['resources']
 
-
-    return json.dumps({})
+    return json.dumps(resourceNames)
 
 
 def hello(environ, start_response, params):
