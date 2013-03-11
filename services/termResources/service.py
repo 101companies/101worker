ac61833226d30365940def166f0042a1e6907b25
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 11:39:43 +0100

    New term resouces module

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -4,10 +4,24 @@ def serveResourceNames(environ, start_response, params):
     status = '200 OK'
     response_headers = [('Content-Type', 'text/json')]
     start_response(status, response_headers)
-    resourceNames = json.load(open('./resources.json'))['resources']
+    resourceNames = json.load(open('./backlinks.json'))['resources']
     return json.dumps({'availableResouces' : resourceNames})
+
+def serveTerm(environ, start_response, params):
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/json')]
+    start_response(status, response_headers)
+    backlinks = json.load(open('./backlinks.json'))['backlinks']
+    term = params.get('term', '')
+    if term in backlinks:
+        return json.dumps({'backlinks' : backlinks[term]})
+    else:
+        return json.dumps({'error' : "No backlinks found")
+
 
 def routes():
     return [
         ('/termResources', serveResourceNames)
+        ('/termResources/(?P<term>.+)', serveTerm),
+        ('/termResources/(?P<term>.+)/(?P<resources>.+)', serveTerm),
     ]
