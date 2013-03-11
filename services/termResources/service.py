876d4acbf65df3892984dc77c0c99096509785c1
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 11:51:01 +0100

    New term resouces module

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -11,10 +11,19 @@ def serveTerm(environ, start_response, params):
     status = '200 OK'
     response_headers = [('Content-Type', 'text/json')]
     start_response(status, response_headers)
-    backlinks = json.load(open('./backlinks.json'))['backlinks']
+    backlinksInfo = json.load(open('./backlinks.json'))
+    backlinks = backlinksInfo['backlinks']
+    resourceNames = backlinksInfo['resources']
     term = params.get('term', '')
+    resource = params.get('resource', '')
     if term in backlinks:
-        return json.dumps({'backlinks' : backlinks[term]})
+        if resource:
+            if resource in resourceNames:
+                return json.dumps({'backlinks' : backlinks[term][resource]})
+            else:
+                return json.dumps({'error' : "Unknown resource " + '"' + resource + '"'})
+        else:
+            return json.dumps({'backlinks' : backlinks[term]})
     else:
         return json.dumps({'error' : "No backlinks found"})
 
