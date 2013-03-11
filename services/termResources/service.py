cb1a963661d797bc946e25e8bc082f2a116c648d
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-11 09:57:01 +0100

    New term resouces module

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -1,27 +1,18 @@
 def helloName(environ, start_response, params):
-    try:
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/json')]
-        start_response(status, response_headers)
-        return {}
-    except Exception as e:
-         return respondError(start_response, error)
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain')]
+    start_response(status, response_headers)
+
+    return 'Hello ' + params['name'] + ', how are you?'
 
 
 def hello(environ, start_response, params):
-    try:
-        status = '200 OK'
-        response_headers = [('Content-Type', 'text/json')]
-        start_response(status, response_headers)
-        return {}
-    except Exception as e:
-         return respondError(start_response, error)
-
-def respondError(start_response, error):
-    status = '500 Internal Server Error'
+    status = '200 OK'
     response_headers = [('Content-Type', 'text/plain')]
     start_response(status, response_headers)
-    return str(error)
+
+    return 'Hello World'
+
 
 def routes():
     return [
