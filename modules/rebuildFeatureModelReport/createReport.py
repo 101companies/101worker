42067065cf6836c42dc87b099144479c7113e593
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-17 21:15:24 +0100

    Changed format for feature-model reports

--- modules/rebuildFeatureModelReport/createReport.py
+++ modules/rebuildFeatureModelReport/createReport.py
@@ -4,9 +4,14 @@ import checker
 
 contribNames = sorted(json.loads(file(sys.argv[1]).read())['dirs'])
 #print checker.check("html5tree")
-report = {}
 for contribName in contribNames:
 	print "Checking " + contribName + " ..."
-	report[contribName] = checker.check(contribName)
-with open(sys.argv[2], 'w+') as reportf:
-  json.dump(report, reportf)
+	report = checker.check(contribName)
+	with open(sys.argv[2] + contribName + '.json', 'w+') as reportf:
+		json.dump(report, reportf)
+	with open(sys.argv[2] + contribName + '.jsonp', 'w+') as reportpf:
+		reportpf.write("callback(")
+		reportpf.write(json.dumps(report))
+		reportpf.write(")")
+
+
