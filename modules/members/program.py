18a75e5e8a0bd2676aebb984e1b469199a065ea9
martinleinberger <martin.leinberger@googlemail.com>
2013-03-18 13:38:35 +0100

    update of members module

--- modules/members/program.py
+++ modules/members/program.py
@@ -9,27 +9,30 @@ import const101
 
 wikiDump = json.load(open(const101.wikiDump, 'r'))
 
-def findNamespaceMembers(namespace):
+def findNamespaceMembers(namespace, instanceof):
     members = []
     for page in wikiDump['wiki']['pages']:
         pageData = page['page']
         if pageData['page']['p'] == namespace:
-            members.append(pageData['page']['n'])
+            if (instanceof and any(instanceof in s for s in pageData.get('internal_links', []))) or not instanceof:
+                members.append(pageData['page']['n'])
+    members.sort()
     return members
 
-def findAndWrite(dirname, namespace):
-    members = findNamespaceMembers(namespace)
+def findAndWrite(dirname, namespace, instanceof=None):
+    members = findNamespaceMembers(namespace, instanceof)
     path = os.path.join(const101.tRoot, dirname)
     if not os.path.exists(path):
         os.mkdir(path)
-    json.dump(members, open(os.path.join(path, 'members.json'), 'w'), indent=4)
+    json.dump(members, open(os.path.join(path, 'members.json'), 'w'))
+
+print "Trying to find namespaces..."
 
 findAndWrite('contributions', 'Contribution')
-findAndWrite('contributors', 'Contributor')
-findAndWrite('languages', 'Language')
-findAndWrite('technologies', 'Technology')
-findAndWrite('themes', 'Theme')
-findAndWrite('vocabularies', 'Vocabulary')
-findAndWrite('concepts', None)
-
-print 'Finished analyzing the wiki'
+findAndWrite('contributors',  'Contributor')
+findAndWrite('languages',     'Language')
+findAndWrite('technologies',  'Technology')
+findAndWrite('themes',        'Theme')
+findAndWrite('vocabularies',  'Vocabulary')
+findAndWrite('concepts',      None)
+findAndWrite('',              'Namespace', 'instanceof::Namespace:Namespace')
