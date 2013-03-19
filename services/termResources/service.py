e3a73c6b3843ed12df24c1918c37ad92cd946b9f
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-19 01:39:31 +0100

    Including mappings

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -7,26 +7,36 @@ def serveResourceNames(environ, start_response, params):
     resourceNames = json.load(open('./backlinks.json'))['resources']
     return json.dumps({'availableResouces' : resourceNames})
 
+def lookup(term, resource, mapping, backlinks):
+    if resource in mapping:
+        if term in mapping[resource]:
+            if mapping[resource][term] in backlinks:
+                return {'backlinks' : backlinks[mapping[resource][term]][resource]}
+            else:
+                return {'error' : "No backlinks found for " + term}
+        else:
+            return {'error' : "No mapping found for " + term}
+    else:
+        return {'error' : "Unknown resource " + '"' + resource + '"'}
+
+
 def serveTerm(environ, start_response, params):
     status = '200 OK'
     response_headers = [('Content-Type', 'text/json')]
     start_response(status, response_headers)
     backlinksInfo = json.load(open('./backlinks.json'))
+    mapping = json.load(open('./mapping.json'))
     backlinks = backlinksInfo['backlinks']
-    resourceNames = backlinksInfo['resources']
     term = params.get('term', '')
     resource = params.get('resource', '')
-    if term in backlinks:
-        if resource:
-            if resource in resourceNames:
-                return json.dumps({'backlinks' : backlinks[term][resource]})
-            else:
-                return json.dumps({'error' : "Unknown resource " + '"' + resource + '"'})
-        else:
-            return json.dumps({'backlinks' : backlinks[term]})
+    if resource:
+        return json.dumps(lookup(term,resource,mapping,backlinks))
     else:
-        return json.dumps({'error' : "No backlinks found"})
-
+        result = {}
+        resourceNames = backlinksInfo['resources']
+        for resource in resourceNames:
+            result[resourceName] = json.dumps(lookup(term,resource,mapping,backlinks))
+        return json.dumps(result)
 
 def routes():
     return [
