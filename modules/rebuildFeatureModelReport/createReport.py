c6c8cbb69c3300d719147f764e5639ed005c78ad
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-19 21:23:55 +0100

    Readding file generation for summary

--- modules/rebuildFeatureModelReport/createReport.py
+++ modules/rebuildFeatureModelReport/createReport.py
@@ -2,15 +2,17 @@ import sys
 import json
 import checker
 
-contribNames = sorted(json.loads(file(sys.argv[1]).read())['dirs'])
-for contribName in contribNames:
-	print "Checking " + contribName + " ..."
-	report = checker.check(contribName)
-	with open(sys.argv[2] + contribName + '.json', 'w+') as reportf:
+def writeReport(report, pathBase):
+	with open(pathBase + '.json', 'w+') as reportf:
 		json.dump(report, reportf)
-	with open(sys.argv[2] + contribName + '.jsonp', 'w+') as reportpf:
-		reportpf.write("callback(")
-		reportpf.write(json.dumps(report))
-		reportpf.write(")")
+	with open(pathBase + '.jsonp', 'w+') as reportpf:
+		reportpf.write('callback(' + json.dumps(report) + ')')
 
-
+contribNames = sorted(json.loads(file(sys.argv[1]).read())['dirs'])
+summary = {}
+for contribName in contribNames:
+	print 'Checking ' + contribName + ' ...'
+	report = checker.check(contribName)
+	writeReport(report, sys.argv[2] + sys.argv[3] + contribName)
+	summary[contribName] = report
+writeReport(summary, sys.argv[2] + 'featureModelReport')
