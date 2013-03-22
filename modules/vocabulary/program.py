314d22b6a6c6e4fa3cf13185f3e8dabde831731d
Kevin Klein <kevin.k1252@gmail.com>
2013-03-22 13:06:05 +0100

    mediawiki api updated for better cleanup and vocabulares added

---
+++ modules/vocabulary/program.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import json
+import urllib2
+import asq
+from asq.initiators import query
+import sys
+import os
+
+sys.path.append('../../libraries/101meta')
+sys.path.append('../../libraries')
+import const101
+import tools101
+from mediawiki import remove_headline_markup
+
+output = os.path.join(const101.tRoot, 'vocabularies')
+output = os.path.abspath(output)
+
+if not os.path.exists(output):
+    os.mkdir(output)
+
+wikidump = json.load(open(sys.argv[1], 'r'))
+pages = wikidump['wiki']['pages']
+
+vocs = (query(pages).where(lambda page: page['page']['page']['p'] == 'Vocabulary')
+    .select(lambda p: p['page']['page']['n']).to_list())
+
+for voc in vocs:
+    voc_name = unicode('instanceof::Vocabulary:' + voc).strip()
+    
+    instances = (query(pages).where(lambda page: voc_name in page['page'].get('internal_links', [])).to_list())
+
+    if not os.path.exists(os.path.join(output, voc)):
+        os.mkdir(os.path.join(output, voc))
+
+    f = open(os.path.join(output, voc, 'members.json'), 'w')
+    data = []
+
+    for instance in instances:
+        data.append({
+            'name': instance['page']['page']['n'],
+            'namespace': instance['page']['page']['p'] or '',
+            'headline': remove_headline_markup(instance['page'].get('headline', ''))
+        })
+
+    json.dump(data, f, indent=4, sort_keys=True)
+        
+    
