bcc58d52d9ab65962116205b9c2114829ddbe4f5
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-26 14:32:08 +0100

    New format

--- services/termResources/service.py
+++ services/termResources/service.py
@@ -42,7 +42,7 @@ def serveTerm(environ, start_response, params):
             backlinks = lookup(term,resource,mapping,backlinks)
             backlinks['name'] = resource
             result.append(backlinks)
-        result = json.dumps(params.get('format', ''))
+        result = json.dumps(result)
     if params.get('format', '') and params.get('format', '') == 'jsonp':
        result = 'callback(' + result + ')'
     return result
