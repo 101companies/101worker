ab74160619db2c90db32f5ff559d26ee5b033fb0
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-31 00:23:13 +0100

    Simple github-link loopup service

--- services/githubLinks/service.py
+++ services/githubLinks/service.py
@@ -6,13 +6,13 @@ def serveLink(environ, start_response, params):
     rawdata = urllib2.urlopen('http://data.101companies.org/dumps/PullRepo.json')
     data = json.load(rawdata)
     if name in data:
-        start_response('200 OK', response_headers)
         result = json.dumps(data[name])
         if iparams.get('format', '') and params.get('format', '') == 'jsonp':
             response_headers = [('Content-Type', 'text/jsonp')]
             result = 'resourcecallback(' + result + ')'
         else:
             response_headers = [('Content-Type', 'text/json')]
+        start_response('200 OK', response_headers)
         return result
     else:
         start_response('404 Resource not found', [('Content-Type', 'text/plain')])
