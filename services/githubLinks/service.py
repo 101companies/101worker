a7e84f7c6fd588571d923f849d6a30f7017c5bbe
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-04-01 17:38:10 +0200

    Added names to github link

--- services/githubLinks/service.py
+++ services/githubLinks/service.py
@@ -6,7 +6,7 @@ def serveLink(environ, start_response, params):
     rawdata = urllib2.urlopen('http://data.101companies.org/dumps/PullRepo.json')
     data = json.load(rawdata)
     if name in data:
-        result = json.dumps({'link': data[name], 'name': 'Source @ Github'})
+        result = json.dumps({'link': data[name], 'name': 'Source at GitHub'})
         if params.get('format', '') and params.get('format', '') == 'jsonp':
             response_headers = [('Content-Type', 'text/jsonp')]
             result = 'githublinkcallback(' + result + ')'
