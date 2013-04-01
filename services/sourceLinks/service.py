2b20b45e05ca587af7d2ca1f1b4f9f5b5827b07b
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-04-01 18:27:31 +0200

    General list of source links

--- services/sourceLinks/service.py
+++ services/sourceLinks/service.py
@@ -6,8 +6,9 @@ def serveLink(environ, start_response, params):
     rawdata = urllib2.urlopen('http://data.101companies.org/dumps/PullRepo.json')
     data = json.load(rawdata)
     if name in data:
-        result = [json.dumps({'link': data[name], 'name': 'Source at GitHub'})]
-        result.append({'link': "http://data.101companies.org/zips/contributions/" + str(name) + ".zip", 'name': "Download .zip"})
+        result = [{'link': data[name], 'name': 'Source at GitHub'}]
+        result.append({'link': "http://data.101companies.org/zips/contributions/" + name + ".zip", 'name': "Download .zip"})
+        result = json.dumps(result)
         if params.get('format', '') and params.get('format', '') == 'jsonp':
             response_headers = [('Content-Type', 'text/jsonp')]
             result = 'githublinkcallback(' + result + ')'
