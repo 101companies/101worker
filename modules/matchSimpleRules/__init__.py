fd659db16114514623741d08be7763df2a37d591
Kevin Klein <kevin.k1252@gmail.com>
2013-05-22 16:22:59 +0200

    first version

---
+++ modules/matchSimpleRules/__init__.py
@@ -0,0 +1,27 @@
+import sys
+import json
+
+sys.path.append('..')
+import interpretRules
+
+sys.path.append('../../libraries/101meta')
+import const101
+import tools101
+
+def main(data):
+    if data['type'] == 'folders':
+        folders = data['data']
+        folders = map(lambda folder: os.path.join('/tmp/', folder), folders)
+        data['data'] = []
+        for folder in folders:
+            for root, subFolders, files in os.walk(folder):
+                for file in files:
+                    f = os.path.join(root,file)
+                    data['data'].append(f)
+
+    rules = json.load(open(rulesDump))
+
+    apply_rules(data['data'], rules, lambda rule: not any(lambda key: key in ('predicate', 'fpredicate', 'content', 'locator'), rule['rule'].keys())
+
+
+
