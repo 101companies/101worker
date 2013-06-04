28a48d6cf7a915e561ce2aff1f8afd0024692288
Kevin Klein <kevin.k1252@gmail.com>
2013-06-04 15:34:12 +0200

    fixed bug in termResources/views.py

--- services_temp/termResources/views.py
+++ services_temp/termResources/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from models import lookup
 import json
+import os
+
+backlinks = os.path.join(os.path.dirname(__file__), 'backlinks.json')
+mappings = os.path.join(os.path.dirname(__file__), 'mapping.json')
 
 def serveResourceNames(request, format):
     isJsonp = request.GET.get('format', '') == 'jsonp'
     
-    resources = json.load(open('./backlinks.json'))['resources']
+    resources = json.load(open(backlinks))['resources']
     result = json.dumps({'availableResouces' : resources})
     if isJsonp:
        result = params.get('callback', 'callback') + '(' + result + ')'
@@ -14,8 +18,8 @@ def serveResourceNames(request, format):
 def serveTerm(request, format, term, resource=""):
     isJsonp = request.GET.get('format', '') == 'jsonp'
     
-    backlinksInfo = json.load(open('./backlinks.json'))
-    mapping = json.load(open('./mapping.json'))
+    backlinksInfo = json.load(backlinks)
+    mapping = json.load(open(mappings))
     backlinks = backlinksInfo['backlinks']
     
     if resource:
