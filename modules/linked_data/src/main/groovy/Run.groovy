51b28794e450d24840674ceb160bd3bb14f587e4
Andrei Varanovich <dotnetby@gmail.com>
2013-06-20 00:40:10 +0200

    Made concepts clickable

--- modules/linked_data/src/main/groovy/Run.groovy
+++ modules/linked_data/src/main/groovy/Run.groovy
@@ -13,11 +13,12 @@ class Run{
 
         println "OO Programming Language"
         def table = ""
-        sortedOOConcepts.each{ k, v -> def r = "<tr><td>${k.getRawVertex().toString()}</td><td>${v}</td><td>${!sortedFpConcepts.containsKey(k)}</td></tr>"; println r; table += r }
+        sortedOOConcepts.each{ k, v -> def r = "<tr><td>${toLink(k)}</td><td>${v}</td><td>${!sortedFpConcepts.containsKey(k)}</td></tr>"; println r; table += r }
         new File("oo.html").withWriter {it << toHtml(table) }
 
+        table = ""
         println "FP Programming Language"
-        sortedFpConcepts.each{ k, v -> def r = "<tr><td>${k.getRawVertex().toString()}</td><td>${v}</td><td>${!sortedOOConcepts.containsKey(k)}</td></tr>"; println r; table += r  }
+        sortedFpConcepts.each{ k, v -> def r = "<tr><td>${toLink(k)}</td><td>${v}</td><td>${!sortedOOConcepts.containsKey(k)}</td></tr>"; println r; table += r  }
         new File("fp.html").withWriter {it << toHtml(table) }
 
         //def uniqueOOConcepts = ooConcepts.unique()
@@ -29,6 +30,12 @@ class Run{
   
         //m.statistics()
     }
+
+    def toLink(v){
+        def uri = v.getRawVertex().toString()
+        def concept = uri.replaceAll("http://101companies.org/resources/concepts/","").replaceAll("_"," ")
+        return "<a href=${uri}>${concept}</a>"
+    }   
 
     def toHtml(table){
         def html = "<html><head></head><body><table style=\"border:1px solid black;border-collapse:collapse;\"><tr><th>Concept</th><th>#Occs</th><th>Unique</th></tr>${table}</table></body></html>"
