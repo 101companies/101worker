e7cdcf21f3b442cd6d24db56a0a96a472eba4d95
martinleinberger <martin.leinberger@googlemail.com>
2013-07-25 00:20:08 +0200

    hopefully final step in retiring services_temp

--- services_temp/helloWorld/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-
-def hello(request):
-
-    return HttpResponse('Hello World', content_type='text/plain')
-
-
--- services_temp/termResourcesCode/models.py
+++
@@ -1,3 +0,0 @@
-from django.db import models
-
-# here could go models
