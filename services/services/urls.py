ccad117636d3cfdfb914f3154ed5f509a1ccd2ac
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-09-22 17:39:06 -0400

    towards clone creator

--- services/services/urls.py
+++ services/services/urls.py
@@ -4,6 +4,7 @@ from explorer import service
 from helloWorld import views as helloWorld
 from sourceLinks import views as sourceLinks
 from termResources import views as termResources
+from cloneCreation import views as cloneCreation
 from termResourcesCode import views as termResourcesCode
 from analyzeSubmission import views as analyzeSubmission
 # Uncomment the next two lines to enable the admin:
@@ -20,7 +21,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    
+
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)(/(?P<path>.*))?/(?P<file>.*\.[^/]+)/(?P<fragment>.+)$', service.serveFileFragment),
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)(/(?P<path>.*))?/(?P<file>.*\.[^/]+)$', service.serveMemberFile),
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)/(?P<path>.+)$', service.serveMemberPath),
@@ -28,22 +29,24 @@ urlpatterns = patterns('',
     (r'^discovery/(?P<namespace>[^/]+)$', service.serveNamespace),
     (r'^discovery$', service.serveAllNamespaces ),
 
-    
+
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)(/(?P<path>.*))?/(?P<file>.*\.[^/]+)/(?P<fragment>.+)/$', service.serveFileFragment),
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)(/(?P<path>.*))?/(?P<file>.*\.[^/]+)/$', service.serveMemberFile),
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)/(?P<path>.+)/$', service.serveMemberPath),
     (r'^discovery/(?P<namespace>[^/]+)/(?P<member>[^/]+)/$', service.serveNamespaceMember),
     (r'^discovery/(?P<namespace>[^/]+)/$', service.serveNamespace),
     (r'^discovery/$', service.serveAllNamespaces ),
-    
+
     (r'^hello$', helloWorld.hello),
-    
+
     (r'^sourceLinks/(?P<name>.+)\.(?P<format>.+)$', sourceLinks.serveLink),
-    
+
     ('^termResources/(?P<term>.+)/(?P<resource>.+)\.(?P<format>.+)$', termResources.serveTerm),
     ('^termResources/(?P<term>.+)\.(?P<format>.+)$', termResources.serveTerm),
     ('^termResources\.(?P<format>.+)$', termResources.serveResourceNames),
     ('^termResourcesCode/(?P<term>.+)/(?P<resName>.+)/(?P<cat>.+)/(?P<resIndex>.+)/(?P<codeIndex>.+)\.(?P<format>.+)$', termResourcesCode.serveCode),
-    
+
+    ('^cloneCreation$', cloneCreation.create),
+
     ('^analyzeSubmission$', analyzeSubmission.analyze)
 )
