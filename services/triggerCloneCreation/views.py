b185fe6cf91abe25716d5d36cf9000e4c1a6f719
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-09-23 23:42:14 -0400

    debugging mode

--- services/triggerCloneCreation/views.py
+++ services/triggerCloneCreation/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 import subprocess
 
 def trigger(request):
+    output = subprocess.Popen(["whoami"], cwd="../../modules/cloneCreation", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    result = output.stdout.read() + "\n"
     output = subprocess.Popen(["make", "run"], cwd="../../modules/cloneCreation", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    result = output.stdout.read()
+    result += output.stdout.read()
     return HttpResponse(result, content_type='text/plain')
 
 
