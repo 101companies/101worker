f7cff8fb91d23d0b2c16a07a8499871bf87936d6
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-09-24 23:01:00 -0400

    adding feature-name detection service

--- services/featureNameDetection/views.py
+++ services/featureNameDetection/views.py
@@ -7,7 +7,7 @@ import subprocess
 from termcolor import colored
 import pysftp
 
-def set_worker(worker, reponame, contribname, sha):
+def set_worker(reponame, contribname, sha):
   print "Preparing worker for commit " + colored(sha, 'magenta')
   subprocess.Popen(["git", "checkout " + sha], cwd="../../../101results/gitdeps/" + reponame, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   print "> " + colored(contribname, 'cyan')
@@ -16,9 +16,9 @@ def set_worker(worker, reponame, contribname, sha):
 
 
 def detect(request):
-  reponame = request.GET.get('reponame', '')
-  contribname = request.GET.get('contribname', '')
-  sha = request.GET.get('sha', '')
+  reponame = request.GET.get('reponame', '').split('/')[0]
+  contribname = request.GET.get('contribname', '').split('/')[0]
+  sha = request.GET.get('sha', '').split('/')[0]
   repo = Repo("101companies", reponame)
   set_worker(reponame, contribname, sha)
   features = detection.detect_all(contribname, sha)
