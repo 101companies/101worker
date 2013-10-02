7b03c95fa8356d0cf9b977e6d5bad1fb0dbde6ac
martinleinberger <martin.leinberger@googlemail.com>
2013-10-02 11:21:01 +0200

    implemented visualization for featureMetricsRewritten

--- modules/featureMetricsRewritten/program.py
+++ modules/featureMetricsRewritten/program.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
 import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 sys.path.append('../../libraries')
 sys.path.append('../../libraries/101meta')
@@ -12,6 +17,7 @@ featuresToInspect = ['total', 'cut']
 contributionWhitelist = ['antlrLexer']
 
 
+#build "Feature" -> ["File 1", "File 2", ...] association
 featureIndex = {}
 contributions = Namespace('contributions')
 
@@ -20,7 +26,7 @@ for folders, files in walk(contributions):
         for feature in file.features:
             featureIndex.setdefault(feature, []).append(file)
 
-
+#build "contributionName" -> "ncloc" association
 contributionIndex = {}
 for feature in featuresToInspect:
     for file in featureIndex.get(feature, []):
@@ -28,4 +34,17 @@ for feature in featuresToInspect:
         if member.name in contributionWhitelist and file.metrics.exists:
             contributionIndex[member.name] = contributionIndex.get(member.name, 0) + file.metrics.ncloc
 
-print contributionIndex
+
+#visualize results
+bars = contributionIndex.values()
+labels = contributionIndex.keys()
+
+figure = plt.figure()
+width = 0.35
+
+ind = np.arange(len(bars))
+plt.bar(ind, bars)
+plt.xticks(ind+width/2.0, labels, rotation=0)
+plt.ylabel('lines of code')
+
+plt.savefig('linesOfCode.png')
