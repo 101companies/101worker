008d643f611ffb5660256adb6630d7de00ca1a18
ruether <ruether@uni-koblenz.de>
2013-11-10 13:21:30 +0100

    Module Validate Module Descriptions added

---
+++ modules/validateModuleDescriptions/validate.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+from os import listdir
+from os.path import isfile, join
+from jsonschema import validate, exceptions 
+import json
+
+json_dump = {
+	"missing_description": [],
+	"invalid_description": []
+	}
+
+schema = json.load(open("moduleDescSchema.json","r"))
+
+for moduleFolder in listdir("../"):
+	if not isfile(moduleFolder):
+		moduleDescPath = join("..",moduleFolder, "module.json")
+		if isfile(moduleDescPath):
+			try:
+				validate(moduleFolder, schema)
+			except exceptions.ValidationError as error:
+				json_dump["invalid_description"].append({"name":moduleFolder, "error_message": error.message})
+		else:
+			json_dump["missing_description"].append(moduleFolder)
+
+dump_file = open("../../../101web/data/dumps/validateModuleDescriptions.json", "w")
+dump_file.write(json.dumps(json_dump, sort_keys=True, indent=4, separators=(',', ': ')))
+dump_file.close()
