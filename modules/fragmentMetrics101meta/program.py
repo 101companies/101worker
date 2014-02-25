6243ecb000abbd22032e39c8ff213d9432ad580e
Nicolas Beck <Nicolas-Beck@gmx.de>
2014-02-25 13:37:59 +0100

    metrics for fragments are only computed iff there exists a fact extractor for this file

--- modules/fragmentMetrics101meta/program.py
+++ modules/fragmentMetrics101meta/program.py
@@ -9,20 +9,22 @@ import tools101
 
 #used for the incrementally stuff
 def testFile(sFilename, tFilename):
-	#return tools101.build(sFilename, tFilename)
-    return True
+    return tools101.build(sFilename, tFilename)
+    # return True
 
 #accept files that have  a geshi code - but also add the relevance of a file with respect to the default relevance
 def testEntry(entry):
 	meta = dict()
 	for m in entry["units"]:
+		if "extractor" in m["metadata"]:
+			meta["extractor"] = m["metadata"]["extractor"]
 		if "geshi" in m["metadata"]:
 			meta["geshi"] = m["metadata"]["geshi"]
 	return meta
 
 # Per-file functionality
 def derive(info, rFilename, sFilename, tFilename1):
-   if "geshi" in info:
+   if "geshi" in info and "extractor" in info:
       tFilename2 = tFilename1[:-len(".fragments.metrics.json")]+".fragments.tokens.json"
       extractorFile = tFilename1[:-len(".fragments.metrics.json")]+".extractor.json"
       print "Process fragments of " + rFilename + " for GeSHi code " + info["geshi"] + "."
@@ -61,4 +63,4 @@ dump = dict()
 dump["geshicodes"] = geshicodes
 dump["numbers"] = dict()
 dump["numbers"]["numberOfGeshicodes"] = len(geshicodes)
-tools101.afterMapMatches(dump, const101.metricsDump)
+tools101.afterMapMatches(dump, const101.fragmentMetricsDump)
