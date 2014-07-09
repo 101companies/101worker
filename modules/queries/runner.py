6efeb4d461f50492dab633feb46e1b06b95f40aa
Andrei Varanovich <dotnetby@gmail.com>
2014-07-09 15:18:48 +0200

    Templates for queries

--- modules/queries/runner.py
+++ modules/queries/runner.py
@@ -6,7 +6,7 @@ from connection import Connection
 from jinja2 import Environment, FileSystemLoader
 
 if __name__=='__main__':
-    env = Environment(loader=FileSystemLoader('tmpl'))
+    env = Environment(loader=FileSystemLoader('templates'))
     connection = Connection('http://triples.101companies.org/openrdf-sesame/')
     connection.use_repository('Testing_2')
 
@@ -20,11 +20,20 @@ if __name__=='__main__':
     for file in file_names:
         js = json.load(open(os.path.join(os.path.dirname(__file__), 'specs', file)))
         query_file = js['query']
-        if query_file == '':
+        template_file = js['template']
+        if query_file == '' or template_file == '':
             continue
         print query_file
         with open(os.path.join(os.path.dirname(__file__), 'sparql', query_file)) as f:
             query = f.read()
             #print query
             res = connection.query(query)
-            print res
+
+            template = env.get_template(template_file)
+            output = template.render(data=res)
+            print output
+
+            with open(os.path.join(os.path.dirname(__file__), 'output', template_file.replace('.tmpl', '.txt')), "w") as output_file:
+                output_file.write(output)
+
+            #print res
