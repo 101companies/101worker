505630b2ab4b8d32f2c7edede5a3e0b266c6335c
Martin Leinberger <martin.leinberger@googlemail.com>
2014-07-21 17:00:17 +0200

    more debugging

--- services/explorer/discovery.py
+++ services/explorer/discovery.py
@@ -133,23 +133,23 @@ def discoverFileFragment(namespace, member, path, file, fragment):
     #gather member data
     lineNumbers = None
     if extractor:
-        try:
-            extractedFacts = DumpdataProvider.getFacts(filePath, extractor)
-            #TODO There has to be a better way to do this
-            for f1 in extractedFacts['fragments']:
-                selected, fragmentPath = find(f1, fragment)
-                if selected:
-                    raise Exception(str(selected))
-                    response['classifier'] = selected['classifier']
-                    response['name'] = selected['name']
-                    if 'startLine' in selected:
-                        raise Exception('Trying to read startLine and endLine with values of {} and {}'.format(selected['startLine'], selected['endLine']))
-                        lineNumbers = {'from':selected['startLine'], 'to':selected['endLine']}
-                    for f2 in selected.get('fragments',[]):
-                        response['fragments'].append(mapFragment(filePath, fragmentPath, f2))
-                    break
-        except:
-            pass
+        #try:
+        extractedFacts = DumpdataProvider.getFacts(filePath, extractor)
+        #TODO There has to be a better way to do this
+        for f1 in extractedFacts['fragments']:
+            selected, fragmentPath = find(f1, fragment)
+            if selected:
+                raise Exception(str(selected))
+                response['classifier'] = selected['classifier']
+                response['name'] = selected['name']
+                if 'startLine' in selected:
+                    raise Exception('Trying to read startLine and endLine with values of {} and {}'.format(selected['startLine'], selected['endLine']))
+                    lineNumbers = {'from':selected['startLine'], 'to':selected['endLine']}
+                for f2 in selected.get('fragments',[]):
+                    response['fragments'].append(mapFragment(filePath, fragmentPath, f2))
+                break
+        #except:
+        #    pass
 
     #gather content
     if lineNumbers or locator:
