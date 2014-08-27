d191b459d3f0d32dda7655b75e0e262a93d18a55
Andrei Varanovich <dotnetby@gmail.com>
2014-08-27 07:43:21 +0200

    Headline fix

--- modules/wiki2json/wiki2json.py
+++ modules/wiki2json/wiki2json.py
@@ -7,63 +7,74 @@ import os
 
 camelize = lambda words: ''.join(words[0].lower() + words[1:])
 
-def extract_properties(internal_links):
-  links = {}
-  for link in internal_links:
-    if not '::' in link: continue
-    l = link.split('::')
-    p = camelize(l[0]) #property
-    n = l[1]
-    if p in links: links[p].append(handle_page_name(n,{}))
-    else: links[p] = [handle_page_name(n,{})]
 
-  return links  
+def extract_properties(internal_links):
+    links = {}
+    for link in internal_links:
+        if not '::' in link: continue
+        l = link.split('::')
+        p = camelize(l[0])  # property
+        n = l[1]
+        if p in links:
+            links[p].append(handle_page_name(n, {}))
+        else:
+            links[p] = [handle_page_name(n, {})]
+
+    return links
+
 
 def handle_page_name(name, props):
-  if name.startswith('http'):
-    return name
-  #print name
-  n = name.split(':')
-  if len(n) == 1:
-    props['p'] = None
-    props['n'] = n[0]
-  else:
-    props['p'] = n[0]
-    props['n'] = n[1]
-  return props
+    if name.startswith('http'):
+        return name
+    # print name
+    n = name.split(':')
+    if len(n) == 1:
+        props['p'] = None
+        props['n'] = n[0]
+    else:
+        props['p'] = n[0]
+        props['n'] = n[1]
+    return props
+
 
 client = MongoClient('db.101companies.org', 27017)
 db = client['wiki_production']
 
-db.authenticate(os.environ['MONGODB_USER'], os.environ['MONGODB_PWD'])
+MONGODB_USER = os.environ['MONGODB_USER']
+MONGODB_PWD = os.environ['MONGODB_PWD']
+
+db.authenticate(MONGODB_USER, MONGODB_PWD)
 
 allPages = []
 for p in db.pages.find():
-  if not p.has_key('page_title_namespace'):
-      continue
-  res = {'headline':'n/a'}
-  handle_page_name(p['page_title_namespace'], res)
+    if not p.has_key('page_title_namespace'):
+        continue
+    if p.has_key('headline'):
+        res = {'headline': p['headline']}
+    else:
+        res = {'headline': 'n/a'}
+    handle_page_name(p['page_title_namespace'], res)
 
-  if 'used_links' in p:
-    res['internal_links'] = p['used_links']
+    if 'used_links' in p:
+        res['internal_links'] = p['used_links']
 
-    properties = extract_properties(res['internal_links']) 
-    for k, v in properties.items():
-      res[k] = v
-  else:
-    res['internal_links'] = {}
+        properties = extract_properties(res['internal_links'])
+        for k, v in properties.items():
+            res[k] = v
+    else:
+        res['internal_links'] = {}
 
-  if 'subresources' in p:
-    res['subresources'] = {}
-    for sr in p['subresources']:
-        for key, value in sr.iteritems():
-            res['subresources'][key] = extract_properties(value)
-            res['subresources'][key]['internal_links'] = value
+    if 'subresources' in p:
+        res['subresources'] = {}
+        for sr in p['subresources']:
+            for key, value in sr.iteritems():
+                res['subresources'][key] = extract_properties(value)
+                res['subresources'][key]['internal_links'] = value
 
-  allPages.append(res)  
+    allPages.append(res)
 
 with open('dump.json', 'w') as f:
-  f.write(json.dumps({'wiki':{'pageCount':len(allPages), 'pages':allPages}}, sort_keys=False))
+    f.write(json.dumps({'wiki': {'pageCount': len(allPages), 'pages': allPages}}, sort_keys=False))
 
 #def url = 'http://101companies.org/endpoint/' + java.net.URLEncoder.encode(obj.label.replaceAll(' ', '_')) + '/summary'
 #                        def json = getJSON(url)
