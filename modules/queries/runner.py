bfc676175ae510c552407ebe444a685c12ebc050
Andrei Varanovich <dotnetby@gmail.com>
2014-08-28 18:12:49 +0200

    Added command line option to run a single file

--- modules/queries/runner.py
+++ modules/queries/runner.py
@@ -5,37 +5,12 @@ from connection import Connection
 from jinja2 import Environment, FileSystemLoader
 import urllib2
 from json import loads
+import sys
 
 SPATXT = 'text/boolean'
 
-if __name__=='__main__':
-    env = Environment(loader=FileSystemLoader('templates'))
-    connection = Connection('http://triples.101companies.org/openrdf-sesame/')
-    connection.use_repository('Testing_2')
 
-    connection.addnamespace('onto', 'http://101companies.org/ontology#')
-    connection.addnamespace('res', 'http://101companies.org/resources#')
-    connection.addnamespace('lang', 'http://101companies.org/resources/Language#')
-
-    req = urllib2.Request('http://triples.101companies.org/openrdf-sesame/repositories/Testing_2/namespaces')
-    req.add_header('Accept', 'application/sparql-results+json')
-    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-    sock = urllib2.urlopen(req)
-    #print "INFO", sock.info()
-    data = sock.read()
-    #print "DATA", data
-    sock.close()
-    prefixes = dict()
-
-    for b in loads(data)['results']['bindings']:
-        prefix = b['prefix']['value']
-        ns = b['namespace']['value']
-        prefixes[ns] = prefix
-
-    relevant_path = os.path.join(os.path.dirname(__file__), 'sparql')
-    included_extenstions = ['sparql']
-    file_names = [fn for fn in os.listdir(relevant_path) if any([fn.endswith(ext) for ext in included_extenstions])]
-
+def process(file_names, prefixes):
     for query_file in file_names:
         template_file = query_file.replace('sparql', 'tmpl')
         if query_file == '' or template_file == '':
@@ -78,3 +53,41 @@ if __name__=='__main__':
                 output_file.write(output)
 
             #print res
+
+if __name__=='__main__':
+
+    env = Environment(loader=FileSystemLoader('templates'))
+    connection = Connection('http://triples.101companies.org/openrdf-sesame/')
+    connection.use_repository('Testing_2')
+
+    connection.addnamespace('onto', 'http://101companies.org/ontology#')
+    connection.addnamespace('res', 'http://101companies.org/resources#')
+    connection.addnamespace('lang', 'http://101companies.org/resources/Language#')
+
+    req = urllib2.Request('http://triples.101companies.org/openrdf-sesame/repositories/Testing_2/namespaces')
+    req.add_header('Accept', 'application/sparql-results+json')
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    sock = urllib2.urlopen(req)
+    #print "INFO", sock.info()
+    data = sock.read()
+    #print "DATA", data
+    sock.close()
+    prefixes = dict()
+
+    for b in loads(data)['results']['bindings']:
+        prefix = b['prefix']['value']
+        ns = b['namespace']['value']
+        prefixes[ns] = prefix
+
+    if len(sys.argv) == 2:
+        print "Processing file: %s" % sys.argv[1]
+        file_names = [sys.argv[1]]
+    else:
+        relevant_path = os.path.join(os.path.dirname(__file__), 'sparql')
+        included_extenstions = ['sparql']
+        file_names = [fn for fn in os.listdir(relevant_path) if any([fn.endswith(ext) for ext in included_extenstions])]
+
+    process(file_names, prefixes)
+
+
+
