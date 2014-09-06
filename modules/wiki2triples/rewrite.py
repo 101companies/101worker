1325c9696f07d29c6e80e1514078934f3ec87505
Andrei Varanovich <dotnetby@gmail.com>
2014-09-06 20:33:13 +0200

    Updated URI for sesame

--- modules/wiki2triples/rewrite.py
+++ modules/wiki2triples/rewrite.py
@@ -254,7 +254,8 @@ def make_general_resource(page, graph):
 
 def main():
     #uri = 'http://triples.101companies.org/openrdf-sesame/repositories/ML_testing'
-    uri = 'http://triples.101companies.org/openrdf-sesame/repositories/Testing_2'
+    #uri = 'http://triples.101companies.org/openrdf-sesame/repositories/Testing_2'
+    uri = 'http://141.26.71.163:8080/openrdf-sesame/repositories/sandbox'
     #clear_sesame_graph(uri)
     #graph = SesameGraph(uri)
     graph = rdflib.Graph()
