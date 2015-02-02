b9e8134d0fb2d391c6be0a55d5667d61fd138b8b
carsten <->
2015-02-02 14:36:20 +0100

    run101meta replaces matches101meta, predicates101meta and fragments101meta.

---
+++ modules/run101meta/program.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from   argparse import ArgumentParser
+import os
+import incremental101
+import meta101
+
+
+parser = ArgumentParser()
+parser.add_argument('--phase', required=True, type=str, help="the 101meta "
+                  + "rules phase (basics, predicates, fragments)")
+args = parser.parse_args()
+
+
+path = os.environ[args.phase + "Dump101"]
+dump = meta101.matchall(args.phase)
+
+incremental101.writejson(path, dump)
