2332da4ef7b6d644a3fa7969f0145ab11368c7c7
carsten <->
2015-02-04 22:57:29 +0100

    Added predicates phase functionality with tests.

--- libraries/meta101/Predicates.py
+++ libraries/meta101/Predicates.py
@@ -1,7 +1,21 @@
-from .Phase import Phase
+import imp
+import os
+from   .Phase import Phase
+from   .util  import tolist
 
 
 class Predicates(Phase):
+
+
+    def __init__(self, *args):
+        super(Predicates, self).__init__(*args)
+        self.predicates = set()
+
+
+    def dump(self):
+        dump = super(Predicates, self).dump()
+        dump["predicates"] = list(self.predicates)
+        return dump
 
 
     def suffix(self):
@@ -10,3 +24,22 @@ class Predicates(Phase):
 
     def applicable(self, rule):
         return "predicate" in rule
+
+
+    def checkpredicate(self, predicate, key, rule, filename, **kwargs):
+        self.predicates.add(predicate)
+        args = tolist(rule["args"]) if "args" in rule else []
+        path = os.path.join(os.environ["repo101dir"], predicate)
+
+        try:
+            # Python cries about a missing parent module if the name contains
+            # dots, so we just replace them with underscores to make it happy.
+            module = imp.load_source(predicate.replace(".", "_"), path)
+            return module.run(args=args, filePath=filename)
+        except Exception as e:
+            self.failures.append({
+                "error" : str(e),
+                "key"   : key,
+                "rule"  : rule,
+            })
+            return None
---
+++ libraries/meta101/t/07_checkpredicates.t.py
@@ -0,0 +1,70 @@
+from TAP.Simple import *
+import os
+import meta101
+
+plan(33)
+
+os.environ["repo101dir"] = os.getcwd()
+pdir        = "t/predicates/"
+noargs      = pdir + "noargs.py"
+using       = pdir + "using.py"
+using1      = pdir + "using1"
+using2      = pdir + "using2"
+nonexistent = pdir + "nonexistent-file"
+phase       = meta101.Predicates()
+
+def testpredicate(comment, rule, success, predicates, failures,
+                  filename="whatever"):
+
+    result = phase.match(0, rule, filename=filename)
+    ok(result, comment) if success else ok(not result, comment)
+    eq_ok(len(phase.predicates), predicates,
+          "...predicate count is {}".format(predicates))
+    eq_ok(len(phase.failures),   failures,
+          "...failure count is {}"  .format(failures))
+
+
+rule = {"predicate" : noargs}
+testpredicate("no arguments predicate matches with no arguments", rule,
+              success=True, predicates=1, failures=0)
+
+rule["args"] = []
+testpredicate("no arguments predicate matches with empty arguments", rule,
+              success=True, predicates=1, failures=0)
+
+rule["args"] = [1, 2 ,3]
+testpredicate("no arguments predicate doesn't match with arguments", rule,
+              success=False, predicates=1, failures=0)
+
+
+rule   = {"predicate" : using, "args" : "somelib"}
+testpredicate("dotNet predicate fails with nonexistent file", rule,
+              success=False, predicates=2, failures=1, filename=nonexistent)
+
+
+testpredicate("dotNet predicate doesn't match 'somelib' on using1", rule,
+              success=False, predicates=2, failures=1, filename=using1)
+
+testpredicate("dotNet predicate matches 'somelib' on using2", rule,
+              success=True, predicates=2, failures=1, filename=using2)
+
+
+rule["args"] = ["otherlib"]
+testpredicate("dotNet predicate matches 'otherlib' on using1", rule,
+              success=True, predicates=2, failures=1, filename=using1)
+
+testpredicate("dotNet predicate doesn't match 'otherlib' on using2", rule,
+              success=False, predicates=2, failures=1, filename=using2)
+
+
+rule["args"] = ["somelib", "otherlib"]
+testpredicate("dotNet predicate matches both libs on using1", rule,
+              success=True, predicates=2, failures=1, filename=using1)
+
+testpredicate("dotNet predicate matches both libs on using2", rule,
+              success=True, predicates=2, failures=1, filename=using2)
+
+
+rule  = {"predicate" : nonexistent}
+testpredicate("match nonexistent predicate fails", rule,
+              success=False, predicates=3, failures=2)
