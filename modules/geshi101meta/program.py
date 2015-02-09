4faeffe02b9a93a052c39b91c9dfcc08e1822c41
carsten <->
2015-02-09 01:12:28 +0100

    Made geshi101meta incremental.

--- modules/geshi101meta/program.py
+++ modules/geshi101meta/program.py
@@ -1,51 +1,25 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
+import os
+import incremental101
+import meta101
 
-import sys
-import simplejson as json
-sys.path.append('../../libraries/101meta')
-import const101
-import tools101
-
-# Per-file functinonality
-def derive(geshicode, rFilename, sFilename, tFilename):
-
-   # Housekeeping for geshicode
-   geshicodes.add(geshicode)
-
-   print "Process " + rFilename + " for GeSHi code " + geshicode + "."
-   command = "php " + geshi + " \"" + sFilename + "\" \"" + tFilename + "\" " + geshicode
-   (status, output) = tools101.run(command)
-
-   # Result aggregation
-   result = dict()
-   result["geshicode"] = geshicode
-   result["command"] = command
-   result["status"] = status
-   result["output"] = output
-
-   return result
-
-
-print "Generating GeSHi-based HTML for 101repo."
-
-if (len(sys.argv) != 2): sys.exit(-1)
-geshi = sys.argv[1] # GeSHi in the workspace
-
-# Initialize housekeeping
+geshi      = os.environ["gatheredGeshi101dir"] + "/run.php"
 geshicodes = set()
-dump = tools101.beforeMapMatches(const101.geshiDump)
-if "geshicodes" in dump:
-   geshicodes = set(dump["geshicodes"])
 
-# Loop over matches
-dump = tools101.deriveByKey("geshi", ".geshi.html", derive)
 
-# Convert set to list before dumping JSON
-geshicodes = list(geshicodes)
+def derive(geshicode, filename, **kwargs):
+    geshicodes.add(geshicode)
 
-# Assemble dump, save it, and exit
-dump = dict()
-dump["geshicodes"] = geshicodes
-dump["numbers"] = dict()
-dump["numbers"]["numberOfGeshicodes"] = len(geshicodes)
-tools101.afterMapMatches(dump, const101.geshiDump)
+    command        = ["php", geshi, filename, "php://stdout", geshicode]
+    status, output = meta101.runcommand(*command)
+
+    if status == 0:
+        return output
+
+    raise RuntimeError("{} exited with {}".format(" ".join(command), status))
+
+
+dump = meta101.derive(key="geshi", suffix=".geshi.html", callback=derive)
+dump["geshicodes"] = list(geshicodes)
+
+incremental101.writejson(os.environ["geshi101dump"], dump)
