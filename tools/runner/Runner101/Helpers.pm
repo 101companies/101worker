2dea801a9f0575396d477fd6a49579c83e283bcf
carsten <->
2015-02-22 17:19:08 +0100

    Removed pointless return code check.

--- tools/runner/Runner101/Helpers.pm
+++ tools/runner/Runner101/Helpers.pm
@@ -13,8 +13,7 @@ use POSIX        qw(strftime);
 sub slurp_json
 {
     my ($path) = @_;
-    my  $json  = slurp $path // die "Can't read $path: $!";
-    decode_json $json
+    decode_json scalar slurp $path;
 }
 
 
