b5c78d8c89523a2c135408d0eaa4a4deec5a7960
hartenfels <hartenfels@uni-koblenz.de>
2015-03-18 21:10:20 +0100

    Touched fragments101meta module.

--- modules/fragments101meta/program.py
+++ modules/fragments101meta/program.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import meta101
+
 # FIXME also check changes on fragments when they get moved into 101worker
 meta101.matchall("fragments", entirerepo=meta101.havechanged(__file__))
