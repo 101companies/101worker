6cc53272740767a1a885505054cde38a92c19ff2
Philipp Seifer <seiferphilipp@gmail.com>
2015-07-06 14:46:03 +0200

    Add new explorer tests and test cases
    - add github url, wiki url, content and member tests
    - add Language/Java/Class HelloWorld test cases

--- services/explorer/tests.py
+++ services/explorer/tests.py
@@ -1,172 +1,206 @@
 from django.test import TestCase, Client
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 import json
 
-class TestMixin(object):
 
+# Basic Test MixIn
+# All test cases inherit at least TestBase and TestCase.
+class TestBase(object):
+
+	# (manual) SetUp() replacement for cases which rely on json_content.
+	def get_json(self):
+		self.response = self.client.get(
+			self.url, 
+			{ 'format': 'json', 'validate': 'true' })
+		self.assertEquals(self.response.status_code, 200)
+		self.json_content = json.loads(self.response.content)
+
+	# Tests how many members exist (return status_code 200).
+	# Asserts zero failures.
+	def something_exists(self, something):
+		self.get_json()
+		broken = 0
+		for thing in self.json_content[something]:
+			try:
+				thing_response = self.client.get(
+					thing['resource'],
+					{ 'format': 'json', 'validate': 'true' })
+				self.assertEquals(thing_response.status_code, 200)
+				thing_json_content = json.loads(thing_response.content)
+				self.assertEquals(thing_json_content['name'], thing['name'])
+			except:
+				broken += 1
+		self.assertIs(broken, 0)
+
+	# Tests basic json template used and response.
 	def test_json(self):
-		response = self.client.get(self.url, { 'format': 'json', 'validate': 'true' })
+		self.get_json()
+		self.assertEquals(self.response.templates, [])
+		self.assertEquals(self.json_content['name'], self.name)
 
-		self.assertEquals(response.status_code, 200)
-		self.assertEquals(response.templates, [])
+	# Tests whether wiki_url is valid and conforms to respective wiki url.
+	def test_wiki(self):
+		self.get_json()
+		self.assertEquals(
+			self.json_content['wiki'], 
+			'http://101companies.org/wiki/' 
+				+ self.json_content['namespace']
+				+ self.wiki)
+		validate = URLValidator()
+		valid_wiki_url = True
+		try:
+			validate(self.json_content['wiki'])
+		except ValidationError, e:
+			valid_wiki_url = False
+		self.assertTrue(valid_wiki_url)
 
-		json_content = json.loads(response.content)
-		self.assertEquals(json_content['name'], self.name)
-		
-
+	# Tests basic html template used and response.
 	def test_html(self):
 		with self.assertTemplateUsed(self.template + '.html'):
-			response = self.client.get(self.url, { 'format': 'html', 'validate': 'true' })
+			html_response = self.client.get(
+				self.url, 
+				{ 'format': 'html', 'validate': 'true' })
+		self.assertEquals(html_response.status_code, 200)
 
-		# self.assertTrue(len(response.context['members']) > 0)
-		self.assertEquals(response.status_code, 200)
-
+	# Tests basic rdf template used and response.
 	def test_rdf(self):
 		with self.assertTemplateUsed(self.template + '.rdf'):
-			response = self.client.get(self.url, { 'format': 'rdf', 'validate': 'true' })
+			rdf_response = self.client.get(
+				self.url, 
+				{ 'format': 'rdf', 'validate': 'true' })
+		self.assertEquals(rdf_response.status_code, 200)
 
-		self.assertEquals(response.status_code, 200)
-
+	# Tests basic ttl template used and response.
 	def test_ttl(self):
 		with self.assertTemplateUsed(self.template + '.ttl'):
-			response = self.client.get(self.url, { 'format': 'ttl', 'validate': 'true' })
+			ttl_response = self.client.get(
+				self.url, 
+				{ 'format': 'ttl', 'validate': 'true' })
+		self.assertEquals(ttl_response.status_code, 200)
 
-		self.assertEquals(response.status_code, 200)
 
-class AllNamespacesTest(TestCase, TestMixin):
+# Tests whether the content (of a fragment) contains certain keywords in a given order.
+class TestContent(object):
+	def test_content_in_order(self):
+		self.get_json()
+		contentstring = self.json_content['content']
+		for content in self.content_contains:
+			index = contentstring.find(content)
+			self.assertTrue(index > -1)
+			contentstring = contentstring[index:]
 
+
+# Tests whether github url exists and is valid.
+class TestGithub(object):
+	def test_github_exists(self):
+		self.get_json()
+		self.assertTrue('https://github.com/' in self.json_content['github'])
+		validate = URLValidator()
+		valid_github_url = True
+		try:
+			validate(self.json_content['github'])
+		except ValidationError:
+			valid_github_url = False
+		self.assertTrue(valid_github_url)
+
+
+# Applied something_exits test for namespaces.
+class TestNamespace(object):
+	def test_members_exist(self):
+		self.something_exists('members')
+
+
+# Applied something_exists test for folders.
+class TestFolder(object):
+	def test_folders_exist(self):
+		self.something_exists('folders')
+
+	def test_files_exist(self):
+		self.something_exists('files')
+
+
+# Applied something_exists test for fragments.
+class TestFragment(object):
+	def test_members_exist(self):
+		self.something_exists('fragments')
+
+
+
+# Root Namespace Test
+
+class AllNamespacesTest(TestBase, TestNamespace, TestCase):
 	template = 'root'
 	url = '/discovery'
 	name = 'Namespace'
+	wiki = ':Namespace'
 
-class SingleNamespacesTest(TestCase, TestMixin):
 
+
+# Contribution - HaskellStarter - Type Company Tests
+
+class ContributionNamespacesTest(TestBase, TestNamespace, TestCase):
 	template = 'namespace'
 	name = 'contributions'
 	url = '/discovery/contributions'
+	wiki = ':Contribution'
 
-class NamespaceMemberTest(TestCase, TestMixin):
-
+class HaskellStarterNamespaceMemberTest(TestBase, TestFolder, TestCase):
 	template = 'folder'
 	name = 'haskellStarter'
 	url = '/discovery/contributions/haskellStarter'
+	wiki = ':haskellStarter'
 
-class NamespaceMemberPathTest(TestCase, TestMixin):
-
+class HaskellStarterNamespaceMemberPathTest(TestBase, TestFolder, TestGithub, TestCase):
 	template = 'folder'
 	name = 'src'
 	url = '/discovery/contributions/haskellStarter/src'
+	wiki = ':haskellStarter'
 
-class FragmentTest(TestCase, TestMixin):
+class HaskellStarterNamespaceMemberFileTest(TestBase, TestFragment, TestGithub, TestCase):
+	template = 'folder'
+	name = 'Main.hs'
+	url = '/discovery/contributions/haskellStarter/src/Main.hs'
+	wiki = ':haskellStarter'
 
+class HaskellStarterCompanyFragmentTest(TestBase, TestFragment, TestGithub, TestContent, TestCase):
 	template = 'fragment'
 	name = 'Company'
 	url = '/discovery/contributions/haskellStarter/src/Main.hs/type/Company'
-
-# class AllNamespacesTest(TestCase):
-
-# 	def test_json(self):
-# 		response = self.client.get('/discovery/', { 'format': 'json', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-# 		self.assertEquals(response.templates, [])
-
-# 		json_content = json.loads(response.content)
-# 		self.assertEquals(json_content['name'], 'Namespace')
-		
-
-# 	def test_html(self):
-# 		with self.assertTemplateUsed('root.html'):
-# 			response = self.client.get('/discovery/', { 'format': 'html', 'validate': 'true' })
-
-# 		self.assertTrue(len(response.context['members']) > 0)
-
-# 	def test_rdf(self):
-# 		with self.assertTemplateUsed('root.rdf'):
-# 			response = self.client.get('/discovery/', { 'format': 'rdf', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-
-# 	def test_ttl(self):
-# 		with self.assertTemplateUsed('root.ttl'):
-# 			response = self.client.get('/discovery/', { 'format': 'ttl', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-
-# class SingleNamespacesTest(TestCase):
-
-# 	def test_json(self):
-# 		response = self.client.get('/discovery/contributions', { 'format': 'json', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-# 		self.assertEquals(response.templates, [])
-
-# 		json_content = json.loads(response.content)
-# 		self.assertEquals(json_content['name'], 'contributions')
-		
-
-# 	def test_html(self):
-# 		with self.assertTemplateUsed('namespace.html'):
-# 			response = self.client.get('/discovery/contributions', { 'format': 'html', 'validate': 'true' })
-
-# 		self.assertTrue(len(response.context['members']) > 0)
-
-# 	def test_rdf(self):
-# 		with self.assertTemplateUsed('namespace.rdf'):
-# 			response = self.client.get('/discovery/contributions', { 'format': 'rdf', 'validate': 'true' })
+	wiki = ':haskellStarter'
+	content_contains = ['type', 'Company', '=']
 
 
-# 	def test_ttl(self):
-# 		with self.assertTemplateUsed('namespace.ttl'):
-# 			response = self.client.get('/discovery/contributions', { 'format': 'ttl', 'validate': 'true' })
 
-# 		self.assertEquals(response.status_code, 200)
+# Language - Java - Class HelloWorld Tests
 
-# class NamespaceMemberTest(TestCase):
+class LanguageNamespacesTest(TestBase, TestNamespace, TestCase):
+	template = 'namespace'
+	name = 'languages'
+	url = '/discovery/languages'
+	wiki = ':Language'
 
-# 	def test_json(self):
-# 		response = self.client.get('/discovery/contributions/haskellStarter/', { 'format': 'json', 'validate': 'true' })
+class JavaNamespaceMemberTest(TestBase, TestFolder, TestCase):
+	template = 'folder'
+	name = 'Java'
+	url = '/discovery/languages/Java'
+	wiki = ':Java'
 
-# 		self.assertEquals(response.status_code, 200)
-# 		self.assertEquals(response.templates, [])
+class JavaNamespaceMemberPathTest(TestBase, TestFolder, TestGithub, TestCase):
+	template = 'folder'
+	name = 'sample'
+	url = '/discovery/languages/Java/sample'
+	wiki = ':Java'
 
-# 		json_content = json.loads(response.content)
-# 		self.assertEquals(json_content['name'], 'haskellStarter')
-		
+class JavaNamespaceMemberFileTest(TestBase, TestFragment, TestGithub, TestCase):
+	template = 'folder'
+	name = 'helloWorld.java'
+	url = '/discovery/languages/Java/sample/helloWorld.java'
+	wiki = ':Java'
 
-# 	def test_html(self):
-# 		with self.assertTemplateUsed('folder.html'):
-# 			response = self.client.get('/discovery/contributions/haskellStarter/', { 'format': 'html', 'validate': 'true' })
-
-# 		self.assertTrue(len(response.context['files']) > 0)
-# 		self.assertTrue(len(response.context['folders']) > 0	)
-
-# 	def test_rdf(self):
-# 		with self.assertTemplateUsed('folder.rdf'):
-# 			response = self.client.get('/discovery/contributions/haskellStarter/', { 'format': 'rdf', 'validate': 'true' })
-
-# class NamespaceMemberPathTest(TestCase):
-
-# 	def test_json(self):
-# 		response = self.client.get('/discovery/contributions/haskellStarter/src', { 'format': 'json', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-# 		self.assertEquals(response.templates, [])
-
-# 		json_content = json.loads(response.content)
-# 		self.assertEquals(json_content['name'], 'src')
-# 		self.assertEquals(json_content['github'], "https://github.com/101companies/101haskell/tree/master/contributions/haskellStarter/src")
-		
-
-# 	def test_html(self):
-# 		with self.assertTemplateUsed('folder.html'):
-# 			response = self.client.get('/discovery/contributions/haskellStarter/src', { 'format': 'html', 'validate': 'true' })
-
-# 		self.assertTrue(len(response.context['files']) > 0)
-# 		self.assertEquals(response.status_code, 200)
-
-# 	def test_rdf(self):
-# 		with self.assertTemplateUsed('folder.rdf'):
-# 			response = self.client.get('/discovery/contributions/haskellStarter/src', { 'format': 'rdf', 'validate': 'true' })
-
-# 		self.assertEquals(response.status_code, 200)
-
+class JavaHelloWorldFragmentTest(TestBase, TestFragment, TestGithub, TestContent, TestCase):
+	template = 'fragment'
+	name = 'HelloWorld'
+	url = '/discovery/languages/Java/sample/helloWorld.java/class/HelloWorld'
+	wiki = ':Java'
+	content_contains = ['class', 'HelloWorld', '{', '}']
