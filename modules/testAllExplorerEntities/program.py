55e71060d0e0b844a6b104290ea17a8669fec6fd
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-07-19 23:15:44 +0200

    Implements basic html view on test outputs
    Saves to 101web/data/views/testAllExplorerEntities.html

--- modules/testAllExplorerEntities/program.py
+++ modules/testAllExplorerEntities/program.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import jinja2
+import json
 import os
 
 # set environment variable that our test is to be ran
 # (is skipped for normal testing as it takes a long time)
-os.environ["TEST_ALL_EXPLORER_ENTITIES"] = "1"
+os.environ['TEST_ALL_EXPLORER_ENTITIES'] = '1'
 
-manage_py = os.path.join(os.environ["worker101dir"], "services", "manage.py")
-os.system("python " + manage_py + " test explorer.testAllExplorerEntities")
+manage_py = os.path.join(os.environ["worker101dir"], 'services', 'manage.py')
+#os.system('python ' + manage_py + ' test explorer.testAllExplorerEntities')
+
+with open('results.json') as infile:
+    data = json.loads(infile.read())
+
+environment = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+template = environment.get_template('main.html')
+
+with open('results.html', 'w') as outfile:
+    outfile.write(template.render(data))
