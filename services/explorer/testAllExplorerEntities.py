a57a05a33b0227710b23cab6933c1345221f1fd2
Lukas Schmelzeisen <l.schmelzeisen@gmx.de>
2015-09-11 20:51:09 +0200

    Change some variable names. Fit code tot PEP and PyCharm recommendations.

--- services/explorer/testAllExplorerEntities.py
+++ services/explorer/testAllExplorerEntities.py
@@ -14,9 +14,6 @@ import discovery
 from mediawiki101 import wikifyNamespace
 
 
-
-
-
 # Descriptions to all possible error types
 # These are output as is after each exception listing, so they may contain html
 # code. Newline are repalce with <br>-Tags automatically
@@ -49,7 +46,6 @@ error_descriptions = {
 }
 
 
-
 class Entity:
     def __init__(self, resource, classifier, name):
         self.resource = self.get_resource_url(resource)
@@ -60,13 +56,13 @@ class Entity:
         return "Entity: (resource: {}, classifier: {}, name: {})".format(
             self.resource, self.classifier, self.name)
 
-    def get_resource_url(self, resource):
+    @staticmethod
+    def get_resource_url(resource):
         url = urlparse(resource)
         resource = url.path
-        if resource[-1:] == '/':
+        if resource[-1] == '/':
             resource = resource[:-1]
         return resource
-
 
 
 class Result:
@@ -79,7 +75,7 @@ class Result:
     # Requires crunch_numbers to be run already
     def __bool__(self):
         return self.total_count
-    __nonzero__=__bool__
+    __nonzero__ = __bool__
 
     def add_result(self, msg):
         entity, error_msg, error_type = msg
@@ -95,64 +91,6 @@ class Result:
             count = len(self.error_dict[error_type])
             self.number_dict[error_type] = count
             self.total_count += count
-
-
-
-
-
-client = Client()
-assigned_resources = multiprocessing.Manager().list()
-ar_mutex = multiprocessing.Lock()
-
-def check_entity(entity):
-    already_assigned = False
-    # MUTEX
-    ar_mutex.acquire()
-    try:
-        if entity.resource in assigned_resources:
-            already_assigned = True
-        else:
-            assigned_resources.append(entity.resource)
-    finally:
-        ar_mutex.release()
-    # MUTEX
-    if already_assigned:
-        return (entity, None, ("ResourceAlreadyAssignedError",
-                               "resource url was already assigned to another entity"))
-    try:
-        # For some reason this call still produces Tracebacks if we catch
-        # the exception further down, how to supress these?
-        response = client.get(entity.resource,
-                              {'format': 'json', 'validate': 'true'})
-
-    except Exception as e:
-        return (entity, None, (type(e).__name__, str(e)))
-
-    if response.status_code != 200:
-        return (entity, None, ("ResponseStatusNotOkError",
-                               "Did not return status ok (200) but instead (" + str(
-                                   response.status_code) + ")"))
-
-    parsed_response = loads(response.content)
-    parsed_classifier = parsed_response['classifier']
-    parsed_name = parsed_response['name']
-    wikified_name = wikifyNamespace(parsed_name)
-
-    if wikified_name and wikified_name != 'None':
-        parsed_name = wikified_name
-
-    if entity.classifier != parsed_classifier:
-        return (entity, None, ("WrongClassifierError",
-                               "Did not have expected classifier '{} but instead '{}'".format(
-                                   entity.classifier, parsed_classifier)))
-    if entity.name != parsed_name:
-        return (entity, None, ("WrongNameError",
-                               "Did not have expected name '{}' but instead '{}'".format(
-                                   entity.name, parsed_name)))
-    return (None, parsed_response, (None, None))
-
-
-
 
 
 class Checker:
@@ -186,7 +124,7 @@ class Checker:
         self.check_file_entities()
         self.check_fragment_entities()
 
-        self.results = {
+        results = {
             'root_errors': self.root_results,
             'namespace_errors': self.namespace_results,
             'member_errors': self.member_results,
@@ -194,43 +132,42 @@ class Checker:
             'file_errors': self.file_results,
             'fragment_errors': self.fragment_results
         }
-        return self.results
-
-
+        return results
 
     def check_root_entities(self):
         rep_errs = self.check_entity_parallel(self.root_entities)
         self.process_result(rep_errs, self.root_results,
-                          self.namespace_entities, 'members')
+                            self.namespace_entities, 'members')
 
     def check_namespace_entities(self):
         rep_errs = self.check_entity_parallel(self.namespace_entities)
         self.process_result(rep_errs, self.namespace_results,
-                          self.member_entities, 'members')
+                            self.member_entities, 'members')
 
     def check_member_entities(self):
         rep_errs = self.check_entity_parallel(self.member_entities)
         self.process_result_two(rep_errs, self.member_results,
-                              self.folder_entities, 'folders',
-                              self.file_entities, 'files')
+                                self.folder_entities, 'folders',
+                                self.file_entities, 'files')
 
     def check_folder_entities(self):
         while True:
             rep_errs = self.check_entity_parallel(self.folder_entities)
             self.folder_entities = []
             self.process_result_two(rep_errs, self.folder_results,
-                                           self.file_entities, 'files',
-                                           self.folder_entities, 'folders')
+                                    self.file_entities, 'files',
+                                    self.folder_entities, 'folders')
             # Loops until no new "level" of folders is found.
             # Since backreferences are caught as ResourceAlreadyAssignedErrors
-            # and thus not added to folder_entitites, this will always terminate.
-            if self.folder_entities == []:
-                break;
+            # and thus not added to folder_entitites, this will always
+            # terminate.
+            if not self.folder_entities:
+                break
 
     def check_file_entities(self):
         rep_errs = self.check_entity_parallel(self.file_entities)
         self.process_result(rep_errs, self.file_results,
-                          self.fragment_entities, 'fragments')
+                            self.fragment_entities, 'fragments')
 
     def check_fragment_entities(self):
         while True:
@@ -238,46 +175,110 @@ class Checker:
             self.fragment_entities = []
             self.process_result(rep_errs, self.fragment_results,
                                 self.fragment_entities, 'fragments')
-            if self.fragment_entities == []:
-                break;
-
+            if not self.fragment_entities:
+                break
 
     # Spawns workers to process source with check_entity and
     # returns the collected results.
     def check_entity_parallel(self, source):
         results = [self.pool.apply_async(check_entity, (entity,))
                    for entity in source]
-        rep_errs = [r.get() for r in results]
+        rep_errs = [result.get() for result in results]
         return rep_errs
 
-
-    def process_result(self, rep_errs, results,
+    @staticmethod
+    def process_result(rep_errs, results,
                        child, child_field):
         for (entity, response, (error_type, error_msg)) in rep_errs:
             if error_type:
                 results.add_result((entity, error_msg, error_type))
             elif response:
-                for ett in response[child_field]:
-                    child.append(Entity(
-                        ett['resource'], ett['classifier'], ett['name']))
+                for child_entity in response[child_field]:
+                    child.append(Entity(child_entity['resource'],
+                                        child_entity['classifier'],
+                                        child_entity['name']))
 
-
-    def process_result_two(self, rep_errs, results,
+    @staticmethod
+    def process_result_two(rep_errs, results,
                            child1, child1_field,
                            child2, child2_field):
         for (entity, response, (error_type, error_msg)) in rep_errs:
             if error_type:
                 results.add_result((entity, error_msg, error_type))
             elif response:
-                for ett in response[child1_field]:
-                    child1.append(Entity(
-                        ett['resource'], ett['classifier'], ett['name']))
-                for ett in response[child2_field]:
-                    child2.append(Entity(
-                        ett['resource'], ett['classifier'], ett['name']))
+                for child_entity in response[child1_field]:
+                    child1.append(Entity(child_entity['resource'],
+                                         child_entity['classifier'],
+                                         child_entity['name']))
+                for child_entity in response[child2_field]:
+                    child2.append(Entity(child_entity['resource'],
+                                         child_entity['classifier'],
+                                         child_entity['name']))
 
 
+assigned_resources = multiprocessing.Manager().list()
+assigned_resources_mutex = multiprocessing.Lock()
+# is the mutex really necessary since assigned_resources already is
+# a multiprocessing list?
 
+# for url querying
+client = Client()
+
+
+# Ideally this function would be a method of class Checker. But this is
+# impossible because multiprocessing can't pickle methods.
+def check_entity(entity):
+    already_assigned = False
+    # MUTEX
+    assigned_resources_mutex.acquire()
+    try:
+        if entity.resource in assigned_resources:
+            already_assigned = True
+        else:
+            assigned_resources.append(entity.resource)
+    finally:
+        assigned_resources_mutex.release()
+    # MUTEX
+    if already_assigned:
+        return entity, None, ("ResourceAlreadyAssignedError",
+                              "resource url was already assigned to "
+                              "another entity")
+
+    try:
+        # For some reason this call still produces Tracebacks if we catch
+        # the exception further down, how to supress these?
+        response = client.get(entity.resource,
+                              {'format': 'json',
+                               'validate': 'true'})
+
+    except Exception as e:
+        return entity, None, (type(e).__name__, str(e))
+
+    if response.status_code != 200:
+        return entity, None, ("ResponseStatusNotOkError",
+                              "Did not return status ok (200) but instead ("
+                              + str(response.status_code) + ")")
+
+    parsed_response = loads(response.content)
+    parsed_classifier = parsed_response['classifier']
+    parsed_name = parsed_response['name']
+    wikified_name = wikifyNamespace(parsed_name)
+
+    if wikified_name and wikified_name != 'None':
+        parsed_name = wikified_name
+
+    if entity.classifier != parsed_classifier:
+        return entity, None, ("WrongClassifierError",
+                              "Did not have expected classifier '{} but "
+                              "instead '{}'".format(
+                                  entity.classifier,
+                                  parsed_classifier))
+    if entity.name != parsed_name:
+        return entity, None, ("WrongNameError",
+                              "Did not have expected name '{}' but "
+                              "instead '{}'".format(entity.name,
+                                                    parsed_name))
+    return None, parsed_response, (None, None)
 
 
 @skipIf("TEST_ALL_EXPLORER_ENTITIES" not in os.environ,
@@ -322,7 +323,7 @@ class AllEntitiesTest(TestCase):
     def tearDownClass(cls):
         pass
 
-    # the following methods just signal to a human test executor were
+    # the following methods just signal to a human test executor where
     # errors happened
 
     def test_root(self):
