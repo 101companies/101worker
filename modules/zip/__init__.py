e4f82701fca86b15f9c100cc77688a46e2b1446d
Kevin <kevin.k1252@gmail.com>
2016-04-18 14:59:32 +0200

    added zip module

---
+++ modules/zip/__init__.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+import os
+import sys
+from distutils.archive_util import make_zipfile
+
+config = {
+    'wantdiff': False
+}
+
+### GENERAL ###
+def ensureFolder(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+def zipFolder(source, target):
+    os.chdir(os.path.dirname(source))
+    make_zipfile(target, './' + os.path.basename(source))
+
+### SPECIFIC ###
+#zip all contributions in single files
+def zipContributions():
+    print "zipping single contribs"
+    dirlist = os.listdir(contrib)
+    for d in dirlist:
+        source = os.path.join(contrib, d)
+        if os.path.isdir(source):
+            target = os.path.join(contribZips, d)
+            zipFolder(source, target)
+
+#zip 101repo folder
+def zip101Repo():
+    print "zipping 101repo folder"
+    source = repo
+    target = os.path.join(webZip, "101repo")
+    zipFolder(source, target)
+
+#zipping dumps
+def zipDumps():
+    print "zipping dumps folder"
+    source = os.path.join(web, "dumps")
+    target = os.path.join(webZip, "dumps")
+    zipFolder(source, target)
+
+#zipping resources
+def zipResources():
+    print "zipping resources folder"
+    source = os.path.join(web, "resources")
+    target = os.path.join(webZip, "resources")
+    zipFolder(source, target)
+
+def run(context):
+    global contrib
+    global web
+    global webZip
+    global contribZips
+    global repo
+
+    ### MAIN PROGRAMM ###
+    #set folders
+    web = os.path.abspath(context.get_env('data101dir'))
+    webZip = os.path.join(web, "zips")
+    contribZips = os.path.join(webZip, "contributions")
+
+    repo = context.get_env('repo101dir')
+    contrib = os.path.join(repo, "contributions")
+
+    #create folders if necessary
+    ensureFolder(webZip)
+    ensureFolder(contribZips)
+
+    #zipping the different parts
+    zipContributions()
+    zip101Repo()
+
+    zipDumps()
+    zipResources()
