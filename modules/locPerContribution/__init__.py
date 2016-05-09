c33361007dc06c8304797f12c7b062e36db71d7a
kevin <kevin@tabolo.de>
2016-05-09 21:31:58 +0200

    added test case for loc per contribution

--- modules/locPerContribution/__init__.py
+++ modules/locPerContribution/__init__.py
@@ -29,29 +29,33 @@ from unittest.mock import Mock, patch
 
 class LocPerContributionTest(unittest.TestCase):
 
+    def setUp(self):
+        self.env = Mock()
+        self.env.read_dump.return_value = { 'python': 45 }
+        self.env.get_derived_resource.return_value = 10
+
     def test_run(self):
         change = {
             'file': 'contributions/python/some-file.py'
         }
-        env = Mock()
-        env.read_dump.return_value = { 'python': 45 }
-        env.get_derived_resource.return_value = 10
+        run(self.env, change)
 
-        run(env, change)
+        self.env.write_dump.assert_called_with('locPerContribution', { 'python': 55 })
 
-        env.write_dump.assert_called_with('locPerContribution', { 'python': 55 })
+    def test_new_contribution(self):
+        change = {
+            'file': 'contributions/ruby/some-file.rb'
+        }
+        run(self.env, change)
+        self.env.write_dump.assert_called_with('locPerContribution', { 'python': 45, 'ruby': 10 })
 
     def test_run_no_contribution(self):
         change = {
             'file': 'something/python/some-file.py'
         }
-        env = Mock()
-        env.read_dump.return_value = { 'python': 45 }
-        env.get_derived_resource.return_value = 10
+        run(self.env, change)
 
-        run(env, change)
-
-        env.write_dump.assert_called_with('locPerContribution', { 'python': 45 })
+        self.env.write_dump.assert_called_with('locPerContribution', { 'python': 45 })
 
 def test():
     suite = unittest.TestLoader().loadTestsFromTestCase(LocPerContributionTest)
